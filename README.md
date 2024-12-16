688fa18a1293c2fd7fe40bcb1e10361bd73fc4be
lauritswl <lauritswl@gmail.com>
2024-12-16 19:01:33 +0100

    Setting up environment for ucloud workflow, deleted scraping scripts as a dataset was found online

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # NLP_IMDb_Exam
+
+## Dataset
+![Dataset found here](https://ieee-dataport.org/open-access/imdb-movie-reviews-dataset)
--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-
 # run using 
 # bash /work/NLP_IMDb_Exam/setup.sh
+
+git config --global user.email "lauritswl@gmail.com"
+git config --global user.name "lauritswl"
+   
 python3 -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip
-pip install -r requirements.txt
+# pip install -r requirements.txt #doesn't work correctly in ucloud
+pip install -r /work/NLP_IMDb_Exam/requirements.txt
